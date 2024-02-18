4a3f494bbe9465fbca5324e58bbcc40ffb3ade8a
Jose Machado <jcmachado@JC-MacBook-Pro.local>
2024-02-18 18:51:30 -0500

    Testing CI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! This is my exercise for Royal')
 })
 
 app.listen(port, () => {
