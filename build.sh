a4821b573507f8ef5ef284f0529a9f0247bb2d05
jcmachado86 <jcmachado86@gmail.com>
2024-02-19 20:56:19 -0500

    Remove app nodes folder

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the current git commit hash
-GIT_COMMIT_HASH=$(git rev-parse HEAD)
-
-# Build the Docker image with the latest tag and the git commit hash as a tag
-docker build --build-arg GIT_COMMIT_HASH=$GIT_COMMIT_HASH -t jcmachado86/royal-app:latest -t jcmachado86/royal-app:$GIT_COMMIT_HASH .
